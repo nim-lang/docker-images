976b4b4a101d94cde5e082ce8ed50f8150394cb3
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2018-07-05 12:34:07 +0200

    adding G++ to regular ubuntu image
    closes #13

--- flavors/regular.nim
+++ flavors/regular.nim
@@ -25,7 +25,7 @@ FROM nimlang/nim:$version-alpine-slim
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apk add --no-cache git
+RUN apk add --no-cache git g++
 #  if version >= "0.16.0":
 RUN cd nim; nim c koch; ./koch tools; \
     ln -s `pwd`/bin/nimble /bin/nimble; \
