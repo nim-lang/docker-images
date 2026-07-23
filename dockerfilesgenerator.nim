2ea8ac086ab8e3730365f190d63cbf01d185309b
Constantine Molchanov <moigagoo@duck.com>
2026-07-23 18:33:38 +0400

    Add linux32 and armv7 archs.

--- dockerfilesgenerator.nim
+++ dockerfilesgenerator.nim
@@ -65,6 +65,8 @@ proc generate() =
     let archMap = {
       "linux_x64": (suffix: "AMD64", dir: "amd64"),
       "linux_arm64": (suffix: "ARM64", dir: "arm64v8"),
+      "linux_x32": (suffix: "I386", dir: "i386"),
+      "linux_armv7l": (suffix: "ARMHF", dir: "arm32v7"),
     }.toTable
 
     var rootContent = templateContent.replace("%%VERSION%%", ver)
