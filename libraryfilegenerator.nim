6f1fe6f6b7069e29ee5fd1b3fe1ddb5c613603c5
Constantine Molchanov <moigagoo@duck.com>
2026-09-08 21:14:22 +0400

    Add 2.2.12

--- libraryfilegenerator.nim
+++ libraryfilegenerator.nim
@@ -5,7 +5,8 @@ let
   gitRepo = "https://github.com/nim-lang/docker-images.git"
 
 let versions = [
-  (version: "2.2.10", tags: @["2.2.10", "2.2", "2", "latest"]),
+  (version: "2.2.12", tags: @["2.2.12", "2.2", "2", "latest"]),
+  (version: "2.2.10", tags: @["2.2.10"]),
   (version: "2.2.8",  tags: @["2.2.8"]),
   (version: "2.2.6",  tags: @["2.2.6"]),
   (version: "2.2.4",  tags: @["2.2.4"]),
